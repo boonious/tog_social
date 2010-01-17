e5e50d17e0f02a2d1dc810648119b5b1ef815a14
Boon Low <boon.low@ed.ac.uk>
2010-01-17 17:18:30 +0000

    add support to display external/federated profile picture (url)

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -2,9 +2,10 @@ module ProfilesHelper
 
   def icon_for_profile(profile, size, options={})
     if profile.icon?
-      photo_url = profile.icon.url(size)
+      federated_icon = profile.federated_profile_image_url
+      photo_url = (profile.icon_file_name=='default_profile.png' && !federated_icon.nil? && federated_icon) || profile.icon.url(size)
       options.merge!(:alt => I18n.t("tog_social.profiles.helper.photo_for_user", :name => profile.full_name))
-      return image_tag(photo_url, options) if photo_url
+      return image_tag(photo_url, options) if photo_url        
     else
       return image_tag("/tog_social/images/#{config["plugins.tog_social.profile.image.default"]}" , options)
     end
