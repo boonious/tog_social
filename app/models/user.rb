356728aadfb3dda7d3df74b9f7b7f7ec08f498a2
Boon Low <boon.low@ed.ac.uk>
2010-02-25 14:46:07 +0000

    add a Group subclass (STI) called Homepage - a personal staging group per user account, modify User model to autmoatically create the homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
 
   before_create :create_profile
+  
+  # Each user owns a homepage (subclass of group)
+  after_create :create_homepage
 
   has_one :profile, :dependent => :destroy
+  has_one :homepage, :dependent => :destroy
   
   has_many :memberships, :dependent => :destroy
   has_many :plain_memberships, :class_name => 'Membership',
@@ -34,6 +38,16 @@ class User < ActiveRecord::Base
 
     def create_profile
       self.profile ||= Profile.new
+    end
+    
+    def create_homepage
+      @group = Homepage.new(:name=> self.login + "'s homepage", :description => "A personal space (homepage) of " + self.login + " where objects can be shared to or from, in various contexts such as posts, links.")
+      @group.author = self
+      @group.private = true
+      @group.save
+      @group.activate!
+      @group.join(self, true)
+      @group.activate_membership(self)
     end
 
 end
